179ce92c690c41fe871065fd23a6ce60f62bfb71
Neel Chetna <neelchetna@gmail.com>
2021-09-20 14:14:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import Signup from "./Components/Signup";
+
+function App() {
+  return (
+    <div class="App-header">
+      Hello World 
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <div>
+            <input type ="text"></input>
+        </div>
+    )
+}
