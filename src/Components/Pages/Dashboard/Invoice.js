3767a0b215048111495c233fdcec2a961fcb095a
neilchetna <neelchetna18@gmail.com>
2022-04-27 21:54:54 +0530

    :sparkles: Invoice feature

---
+++ src/Components/Pages/Dashboard/Invoice.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Invoice() {
+  return (
+    <div className="w-full h-full">
+      <iframe
+        className="w-full h-full"
+        title="react-invoice"
+        src="https://tuanpham-dev.github.io/react-invoice-generator/"
+      />
+    </div>
+  );
+}
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Invoice from "./Pages/Dashboard/Invoice";
 
 import Ledger from "./Pages/Dashboard/Ledger/Ledger";
 import Settings from "./Pages/Dashboard/Settings";
@@ -9,6 +10,7 @@ function Routes() {
     <Switch>
       <Route path="/ledger" exact component={Ledger} />
       <Route path="/Settings" exact component={Settings} />
+      <Route path="/invoice" exact component={Invoice} />
     </Switch>
   );
 }
